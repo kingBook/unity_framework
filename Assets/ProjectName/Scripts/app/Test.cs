40d3a0f1d2aaf356455a6845ab7cb1d2b11ff7a3
kingBook <412126604@qq.com>
2019-06-10 19:45:49 +0800

    update 2019-06-10 19:45

--- Assets/ProjectName/Scripts/app/Test.cs
+++ Assets/ProjectName/Scripts/app/Test.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Test:MonoBehaviour {
 
 	private void Start() {
 		//SpriteRenderer renderer=createObjAddComponent<SpriteRenderer>("testObj");
 		//Debug.Log(renderer);//output: testObj (UnityEngine.SpriteRenderer)
+		Invoke("loadLevelScene",2);
 	}
 
 	private T createObjAddComponent<T>(string name)where T:Component{
 		GameObject obj=new GameObject(name);
 		T component=obj.AddComponent<T>();
 		return component;
+	}
+
+
+	private void loadLevelScene(){
+		SceneManager.LoadSceneAsync("level",LoadSceneMode.Additive);
 	}
 }
