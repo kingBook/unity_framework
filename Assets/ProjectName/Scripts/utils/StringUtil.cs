f6e95766923c5c6887c5127e50c1e6bd8ad781cc
kingBook <412126604@qq.com>
2019-06-13 17:49:34 +0800

    update 2019-06-13 17:49

---
+++ Assets/ProjectName/Scripts/utils/StringUtil.cs
@@ -0,0 +1,36 @@
+﻿namespace framework{
+	using UnityEngine;
+	using System.Collections;
+
+	public class StringUtil{
+		public static bool CustomEndsWith(string a, string b) {
+            int ap = a.Length - 1;
+            int bp = b.Length - 1;
+
+            while (ap >= 0 && bp >= 0 && a [ap] == b [bp]) {
+                ap--;
+                bp--;
+            }
+            return (bp < 0 && a.Length >= b.Length) || 
+
+                    (ap < 0 && b.Length >= a.Length);
+            }
+
+        public static bool CustomStartsWith(string a, string b) {
+            int aLen = a.Length;
+            int bLen = b.Length;
+            int ap = 0; int bp = 0;
+
+            while (ap < aLen && bp < bLen && a [ap] == b [bp]) {
+            ap++;
+            bp++;
+            }
+
+            return (bp == bLen && aLen >= bLen) || 
+
+                    (ap == aLen && bLen >= aLen);
+        } 
+
+		
+	}
+}
