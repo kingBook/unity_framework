7cc755302f05d449b2b55163c204ca6c54e97fa3
kingBook <412126604@qq.com>
2019-07-05 17:47:38 +0800

    update 2019-07-05 17:47

--- Assets/Scripts/utils/Debug2.cs
+++ Assets/Scripts/utils/Debug2.cs
@@ -7,10 +7,14 @@ public static class Debug2 {
 	//如果传递给 Conditional 属性的任何定义均未被定义，则会被修饰的方法以及对被修饰方法的所有调用都会在编译中剔除
 	[Conditional("UNITY_EDITOR")]
 	public static void Log(params object[] args) {
-		string format="";
-		for(int i=0;i<args.Length;i++){
-			format+="{"+i+"}"+(i<args.Length-1?",":"");
+		int len=args.Length;
+		object obj;
+		string str="";
+		for(int i=0;i<len;i++){
+			obj=args[i];
+			str+=(obj==null)?"Null":obj.ToString();
+			if(i<len-1)str+=' ';
 		}
-		UnityEngine.Debug.LogFormat(format,args);
+		UnityEngine.Debug.Log(str);
 	}
 }
