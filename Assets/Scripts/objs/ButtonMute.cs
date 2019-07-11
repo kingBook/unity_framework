1bc708d3d25a431cd741101576cb26c1ba0b331d
kingBook <412126604@qq.com>
2019-07-11 17:55:24 +0800

    update 2019-07-11 17:55

---
+++ Assets/Scripts/objs/ButtonMute.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(SwapButtonImage))]
+public class ButtonMute:BaseMonoBehaviour{
+	
+	protected override void Awake() {
+		base.Awake();
+		SwapButtonImage swapButtonImage=GetComponent<SwapButtonImage>();
+		swapButtonImage.swapTo(AudioListener.pause?1:0);
+	}
+
+}
---
+++ Assets/Scripts/objs/SwapButtonImage.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 点击按钮时，切换按钮上的图片
+/// </summary>
+public class SwapButtonImage:BaseMonoBehaviour{
+	[Tooltip("来回切换的两张图片")]
+	public Sprite[] sprites;
+	private Button _button;
+	private Image _image;
+	protected override void Awake() {
+		base.Awake();
+		_button=GetComponent<Button>();
+		_button.onClick.AddListener(onClick);
+
+		_image=GetComponent<Image>();
+	}
+
+	private void onClick(){
+		if(_image.sprite==sprites[0]){
+			_image.sprite=sprites[1];
+		}else{
+			_image.sprite=sprites[0];
+		}
+	}
+
+	public void	swapTo(int spriteId){
+		_image.sprite=sprites[spriteId];
+	}
+
+	protected override void OnDestroy() {
+		_button.onClick.RemoveListener(onClick);
+		base.OnDestroy();
+	}
+}
