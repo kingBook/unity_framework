630f86e39e58a4e564087d1a8769d7322af6288c
kingBook <412126604@qq.com>
2019-07-12 14:41:48 +0800

    update 2019-07-12 14:41

--- Assets/Scripts/objs/ButtonMute.cs
+++ Assets/Scripts/objs/ButtonMute.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 [RequireComponent(typeof(SwapButtonImage))]
 public class ButtonMute:BaseMonoBehaviour{
 	
-	protected override void Awake() {
-		base.Awake();
+	protected override void Start() {
+		base.Start();
 		SwapButtonImage swapButtonImage=GetComponent<SwapButtonImage>();
 		swapButtonImage.swapTo(AudioListener.pause?1:0);
 	}
