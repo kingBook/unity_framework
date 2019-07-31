98024ddc1b229ef5cd5d49df1e0b21556c0ad621
kingBook <412126604@qq.com>
2019-07-31 11:49:16 +0800

    update 2019-07-31 11:49

--- Assets/Scripts/core/SceneLoader.cs
+++ Assets/Scripts/core/SceneLoader.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Events;
 /// <summary>
 /// 场景加载器
 /// </summary>
@@ -10,6 +11,8 @@ public sealed class SceneLoader:BaseMonoBehaviour{
 	public Image imageMid;
 	[Tooltip("百分比文本框")]
 	public Text txt;
+	[Tooltip("场景加完成后，是否调用SceneManager.SetActiveScene(scene)激活场景")]
+	public bool isSetActiveScene=true;
 
 	private AsyncOperation _asyncOperation;
 
@@ -21,34 +24,42 @@ public sealed class SceneLoader:BaseMonoBehaviour{
 		gameObject.SetActive(false);
 	}
 
+	protected override void OnEnable() {
+		base.OnEnable();
+		SceneManager.sceneLoaded+=onSceneLoaded;
+	}
+
 	/// <summary>
 	/// Additive模式同步加载场景
 	/// </summary>
-	/// <param name="sceneName"></param>
+	/// <param name="sceneName">场景在BuildSettings窗口的路径或名称</param>
 	public void load(string sceneName){
 		load(sceneName,LoadSceneMode.Additive);
 	}
 	/// <summary>
 	/// 同步加载场景
 	/// </summary>
-	/// <param name="sceneName"></param>
-	/// <param name="mode"></param>
+	/// <param name="sceneName">场景在BuildSettings窗口的路径或名称</param>
+	/// <param name="mode">加载模式</param>
 	public void load(string sceneName,LoadSceneMode mode){
 		SceneManager.LoadScene(sceneName,mode);
+		//为了能够侦听场景加载完成时设置为激活场景,所以激活
+		gameObject.SetActive(true);
+		imageMid.fillAmount=1;
 	}
 
 	/// <summary>
 	/// Additive模式异步加载场景，将显示进度条
 	/// </summary>
-	/// <param name="sceneName"></param>
+	/// <param name="sceneName">场景在BuildSettings窗口的路径或名称</param>
 	public void loadAsync(string sceneName){
 		loadAsync(sceneName,LoadSceneMode.Additive);
 	}
 	/// <summary>
 	/// 异步加载场景，将显示进度条
 	/// </summary>
-	/// <param name="sceneName"></param>
-	/// <param name="mode"></param>
+	/// <param name="sceneName">场景在BuildSettings窗口的路径或名称</param>
+	/// <param name="mode">加载模式,默认为：LoadSceneMode.Additive</param>
 	public void loadAsync(string sceneName,LoadSceneMode mode){
 		gameObject.SetActive(true);
 		imageMid.fillAmount=0;
@@ -57,7 +68,7 @@ public sealed class SceneLoader:BaseMonoBehaviour{
 
 	IEnumerator loadSceneAsync(string sceneName,LoadSceneMode mode){
 		_asyncOperation=SceneManager.LoadSceneAsync(sceneName,mode);
-		_asyncOperation.completed+=onComplete;
+		_asyncOperation.completed+=onAsyncComplete;
 		_asyncOperation.allowSceneActivation=false;
 		while(!_asyncOperation.isDone){
 			float progress=_asyncOperation.progress;
@@ -73,15 +84,27 @@ public sealed class SceneLoader:BaseMonoBehaviour{
 		}
 	}
 
-	private void onComplete(AsyncOperation asyncOperation){
+	private void onAsyncComplete(AsyncOperation asyncOperation){
 		gameObject.SetActive(false);
-		_asyncOperation.completed-=onComplete;
+		_asyncOperation.completed-=onAsyncComplete;
 		_asyncOperation=null;
+	}
+
+	private void onSceneLoaded(Scene scene,LoadSceneMode mode){
+		if(isSetActiveScene){
+			SceneManager.SetActiveScene(scene);
+		}
+		gameObject.SetActive(false);
+	}
+
+	protected override void OnDisable() {
+		SceneManager.sceneLoaded-=onSceneLoaded;
+		base.OnDisable();
 	}
 
 	protected override void OnDestroy(){
 		if(_asyncOperation!=null){
-			_asyncOperation.completed-=onComplete;
+			_asyncOperation.completed-=onAsyncComplete;
 		}
 		base.OnDestroy();
 	}
