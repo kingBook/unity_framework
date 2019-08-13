224129d43a4093b1b763fdf19ec58867d6d4f653
kingBook <412126604@qq.com>
2019-08-13 12:13:22 +0800

    update 2019-08-13 12:13

--- Assets/Scripts/utils/FuncUtil.cs
+++ Assets/Scripts/utils/FuncUtil.cs
@@ -1,8 +1,69 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+/// <summary>
+/// 函数工具类
+/// </summary>
 public class FuncUtil{
+	
+	/// <summary>
+	/// 返回包围盒的角点列表
+	/// </summary>
+	/// <param name="boundsCenter">包围盒的中心</param>
+	/// <param name="boundsExtents">Bounds.extents</param>
+	/// <returns></returns>
+	public static Vector3[] getBoundsCorners(Vector3 boundsCenter,Vector3 boundsExtents){
+		Vector3[] vertices=new Vector3[8];
+		//左下后
+		vertices[0]=boundsCenter+Vector3.Scale(boundsExtents,new Vector3(-1,-1,-1));
+		//左上后
+		vertices[1]=boundsCenter+Vector3.Scale(boundsExtents,new Vector3(-1,1,-1));
+		//右上后
+		vertices[2]=boundsCenter+Vector3.Scale(boundsExtents,new Vector3(1,1,-1));
+		//右下后
+		vertices[3]=boundsCenter+Vector3.Scale(boundsExtents,new Vector3(1,-1,-1));
+
+		//左下前
+		vertices[4]=boundsCenter+Vector3.Scale(boundsExtents,new Vector3(-1,-1,1));
+		//左上前
+		vertices[5]=boundsCenter+Vector3.Scale(boundsExtents,new Vector3(-1,1,1));
+		//右上前
+		vertices[6]=boundsCenter+Vector3.Scale(boundsExtents,new Vector3(1,1,1));
+		//右下前
+		vertices[7]=boundsCenter+Vector3.Scale(boundsExtents,new Vector3(1,-1,1));
+		return vertices;
+	}
+    
+    /// <summary>
+    /// 将世界坐标点数组投射到以原点为中心的平面
+    /// </summary>
+    /// <param name="points">世界坐标点数组</param>
+    /// <param name="pointCount">坐标点数量</param>
+    /// <param name="planeNormal">平面法线</param>
+    /// <returns></returns>
+    public static Vector3[] worldPointsToPlane(Vector3[] points,int pointCount,Vector3 planeNormal){
+        for(int i=0;i<pointCount;i++){
+            var vertex=points[i];
+            points[i]=Vector3.ProjectOnPlane(vertex,planeNormal);
+        }
+        return points;
+    }
+    
+    /// <summary>
+    /// 将世界坐标点数组转换为屏幕坐标
+    /// </summary>
+    /// <param name="points">世界坐标点数组</param>
+    /// <param name="pointCount">坐标点数量</param>
+    /// <param name="camera">用于转换的相机</param>
+    /// <returns></returns>
+	public static Vector3[] worldPointsToScreen(Vector3[] points,int pointCount,Camera camera){
+		for(int i=0;i<pointCount;i++){
+            var vertex=points[i];
+            points[i]=camera.WorldToScreenPoint(vertex);
+        }
+		return points;
+	}
+
 	/// <summary>
 	/// 获取DontDestroyOnLoad的所有游戏对象
 	/// <br>注意：这个方法很低效</br>
