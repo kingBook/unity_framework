b2eaa08c59a96fea0109afe3276e28cf4c6f3337
kingBook <412126604@qq.com>
2019-08-23 10:55:24 +0800

    update 2019-08-23 10:55

---
+++ Assets/Scripts/utils/PhysicsUtil.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsUtil{
+	/// <summary>
+	/// 返回离射线原点最近的RaycastHit,如果没有找到将返回new RaycastHit()
+	/// </summary>
+	/// <param name="ray">射线</param>
+	/// <param name="layerMask">用于射线计算的LayerMask，如：LayerMask.GetMask("ItemModel")。</param>
+	/// <returns></returns>
+	public static RaycastHit getClosestRaycastHit(Ray ray,int layerMask){
+		RaycastHit result=new RaycastHit();
+		RaycastHit[] hits=Physics.RaycastAll(ray,Mathf.Infinity,layerMask);
+		float minDistance=float.MaxValue;
+		int len=hits.Length;
+		for(int i=0;i<len;i++){
+			RaycastHit hit=hits[i];
+			if(hit.distance<minDistance){
+				minDistance=hit.distance;
+				result=hit;
+			}
+		}
+		return result;
+	}
+	
+}
