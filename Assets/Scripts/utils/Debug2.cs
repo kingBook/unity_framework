edbeefec0667427b7c0f8137f31a679801b15470
kingBook <412126604@qq.com>
2019-08-28 17:45:32 +0800

    update 2019-08-28 17:45

--- Assets/Scripts/utils/Debug2.cs
+++ Assets/Scripts/utils/Debug2.cs
@@ -1,8 +1,12 @@
 ﻿using System.Diagnostics;
+using UnityEngine;
 /// <summary>
-/// 自定义的Log类，解决Debug.Log()/Debug.LogFormat()多参数不方便和在发布版本Log不剔除问题
+/// 自定义的Log类
+/// <br>1.解决Debug.Log()/Debug.LogFormat()多参数不方便和在发布版本Log不剔除问题</br>
 /// <br>具体描述：https://docs.unity3d.com/Manual/BestPracticeUnderstandingPerformanceInUnity7.html (Debug code & the [conditional] attribute部分)</br>
+/// <br>2.解决Vector3/Vector2很小的小数输出为0，如(1,6.167067E-11,-1.241278E-08)</br>
 /// </summary>
+/// 
 public static class Debug2 {
 	//如果传递给 Conditional 属性的任何定义均未被定义，则会被修饰的方法以及对被修饰方法的所有调用都会在编译中剔除
 	[Conditional("UNITY_EDITOR")]
@@ -12,7 +16,15 @@ public static class Debug2 {
 		string str="";
 		for(int i=0;i<len;i++){
 			obj=args[i];
-			str+=(obj==null)?"Null":obj.ToString();
+			if(obj is Vector3){
+				Vector3 v3=(Vector3)obj;
+				str+=string.Format("({0},{1},{2})",v3.x,v3.y,v3.z);
+			}else if(obj is Vector2){
+				Vector2 v2=(Vector2)obj;
+				str+=string.Format("({0},{1})",v2.x,v2.y);
+			}else{
+				str+=(obj==null)?"Null":obj.ToString();
+			}
 			if(i<len-1)str+=' ';
 		}
 		UnityEngine.Debug.Log(str);
