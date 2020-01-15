c9444f54c47685b4fe37ad7c68d4554eac966275
kingBook <412126604@qq.com>
2020-01-15 17:38:49 +0800

    update 2020-01-15 17:38

--- Assets/Plugins/framework/utils/FuncUtil.cs
+++ Assets/Plugins/framework/utils/FuncUtil.cs
@@ -123,4 +123,37 @@ public class FuncUtil{
 		return result; 
 	}
 	
+	/// <summary>
+	/// 返回一个游戏对象的包围盒(将跳过计算未激活的Renderer)
+	/// </summary>
+	/// <param name="gameObject">游戏对象</param>
+	/// <param name="filterChildren">需要过滤的子级对象</param>
+	/// <returns></returns>
+	public static Bounds GetGameObjectBounds(GameObject gameObject,params GameObject[]filterChildren){
+		int filterChildCount=filterChildren.Length;
+		Renderer[] filterChildRenderers=new Renderer[filterChildCount];
+		for(int i=0;i<filterChildCount;i++){
+			filterChildRenderers[i]=filterChildren[i].GetComponent<Renderer>();
+		}
+		
+		Bounds bounds=new Bounds();
+		Renderer rootRenderer=gameObject.GetComponent<Renderer>();
+		if(rootRenderer!=null&&rootRenderer.enabled){
+			bounds=rootRenderer.bounds;
+		}
+		Renderer[] subRenderers=gameObject.GetComponentsInChildren<Renderer>();
+		int j=subRenderers.Length;
+		while(--j>=0){
+			Renderer renderer=subRenderers[j];
+			if(!renderer.enabled)continue;
+			if(Array.IndexOf(filterChildRenderers,renderer)>-1)continue;
+			if(bounds.min.magnitude==0f && bounds.max.magnitude==0f){
+				bounds=renderer.bounds;
+			}else{
+				bounds.Encapsulate(renderer.bounds);
+			}
+		}
+		return bounds;
+	}
+	
 }
