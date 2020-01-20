a2acfd66d641ec79a93b8c51adfbaf4926bdc687
kingBook <412126604@qq.com>
2020-01-20 21:28:59 +0800

    update 2020-01-20 21:28

--- Assets/Plugins/framework/core/ScreenLog.cs
+++ Assets/Plugins/framework/core/ScreenLog.cs
@@ -9,6 +9,7 @@ public class ScreenLog:BaseMonoBehaviour{
 	private string m_output="";
 	private Vector2 m_scrollPos;
 	private bool m_isPause;
+	private bool m_isMinimized;
 	
 	protected override void OnEnable(){
 		base.OnEnable();
@@ -17,27 +18,37 @@ public class ScreenLog:BaseMonoBehaviour{
 	
 	protected override void OnGUI2(){
 		base.OnGUI2();
-		float width=Screen.width*0.3f;
-		float height=Screen.height;
-		GUILayout.BeginVertical();
-			//滚动的文本
-			m_scrollPos=GUILayout.BeginScrollView(m_scrollPos,GUILayout.Height(height-30f));
-			GUILayout.TextArea(m_output,GUILayout.MaxWidth(width),GUILayout.ExpandHeight(true));
-			GUILayout.EndScrollView();
-			
-			GUILayout.BeginHorizontal();
-				GUILayout.FlexibleSpace();
-				//暂停/恢复按钮
-				string pauseResumeText=m_isPause?"Resume":"Pause";
-				if(GUILayout.Button(pauseResumeText)){
-					m_isPause=!m_isPause;
-				}
-				//清除按钮
-				if(GUILayout.Button("Clear")){
-					m_output="";
-				}
-			GUILayout.EndHorizontal();
-		GUILayout.EndVertical();
+		float buttonSize=Screen.height*0.1f;
+		if(m_isMinimized){ 
+			if(GUILayout.Button(" > ",GUILayout.MinWidth(buttonSize),GUILayout.MinHeight(buttonSize))){
+				m_isMinimized=false;
+			}
+		}else{
+			float width=Screen.width*0.3f;
+			float height=Screen.height;
+			GUILayout.BeginVertical();
+				//滚动的文本
+				m_scrollPos=GUILayout.BeginScrollView(m_scrollPos);
+				GUILayout.TextArea(m_output,GUILayout.MaxWidth(width),GUILayout.ExpandHeight(true));
+				GUILayout.EndScrollView();
+				
+				GUILayout.BeginHorizontal();
+					//最小化按钮
+					if(GUILayout.Button(" < ",GUILayout.MinHeight(buttonSize))){
+						m_isMinimized=true;
+					}
+					//暂停/恢复按钮
+					string pauseResumeText=m_isPause?"Resume":"Pause";
+					if(GUILayout.Button(pauseResumeText,GUILayout.MinHeight(buttonSize))){
+						m_isPause=!m_isPause;
+					}
+					//清除按钮
+					if(GUILayout.Button("Clear",GUILayout.MinHeight(buttonSize))){
+						m_output="";
+					}
+				GUILayout.EndHorizontal();
+			GUILayout.EndVertical();
+		}
 	}
 	
 	private void LogHandler(string logString, string stackTrace, LogType type){
