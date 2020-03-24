5703976625beabbc9d5c6e65657987cb071649ae
kingBook <412126604@qq.com>
2020-03-24 17:42:30 +0800

    2020-03-24 删除原来的进入播放模式编辑器扩展,改用EditorSceneManager.playModeStartScene设置

--- Assets/Editor/PlayBeforeData.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-/// <summary>
-/// 记录播放前的数据
-/// </summary>
-public class PlayBeforeData:ScriptableObject{
-		public string activeScenePath;
-		public bool isOpenedMainScene;
-	}
