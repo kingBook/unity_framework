d9049851db36dfcbc1142619e9ccde9fbcd78ded
kingBook <412126604@qq.com>
2020-04-26 17:27:14 +0800

    update 2020-04-26 17:27

---
+++ Assets/Plugins/Framework/Objs/Cars/TopViewCar.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+
+#pragma warning disable 0649
+
+/// <summary> 俯视的车 </summary>
+public class TopViewCar:BaseMonoBehaviour{
+	public float maxSpeed=15;				//移动的最大速度
+	public float maxWheelRotation=45;		//轮子的最大旋转角
+	public float length=100;				//车身长
+	public bool isResetWheelRotation;		//在未控制车轮旋转的情况下，是否重置轮子的旋转角与车身一致
+	[Range(0.0001f,1)]                  	
+	public float resetWheelRate=0.5f;		//重置轮子旋转角的速率
+	[Range(0,1)]                        	
+	public float speedDamp=0.5f;			//速度惯性衰减[0,1]
+	public Transform body;					//车的Transfrom
+	public Transform[] frontWheels;			//前轮Transform列表(可选的)
+	public Transform[] rearWheels;			//前轮Transform列表(可选的)
+											
+	private float m_speed;					//移动速度
+	private float m_wheelRotation;			//轮子的旋转角<角度>
+	private bool m_isDriveing;				//是否正在驱动中...
+	private bool m_isResetWheeling;			//是否正在重置轮子旋转角...
+
+	protected override void FixedUpdate2(){
+		base.FixedUpdate2();
+		//轮子的旋转角<弧度>
+		float wheelRadian=m_wheelRotation*Mathf.Deg2Rad;
+		//当前移动速度在当前轮子方向上的分量（x：用于计算车向前的位移 y：用于计算车的旋转）。
+		Vector2 speedDelta=new Vector2(m_speed*Mathf.Cos(wheelRadian),m_speed*Mathf.Sin(wheelRadian));
+		//旋转车身
+		float rotationDeltaY=Mathf.Atan2(speedDelta.y,length)*Mathf.Rad2Deg;
+		Vector3 bodyEulerAngles=body.eulerAngles;
+		bodyEulerAngles.y+=rotationDeltaY;
+		body.eulerAngles=bodyEulerAngles;
+		//按车身的当前的Y旋转角进行移动
+		float bodyRadianY=bodyEulerAngles.y*Mathf.Deg2Rad;
+		float vx=speedDelta.x*Mathf.Cos(bodyRadianY)*Time.deltaTime;
+		float vz=-speedDelta.x*Mathf.Sin(bodyRadianY)*Time.deltaTime;
+		body.Translate(vx,0,vz,Space.World);
+		//旋转前轮
+		int i=frontWheels.Length;
+		while(--i>=0){
+			Transform frontWheel=frontWheels[i];
+			Vector3 frontWheelEulerAngles=frontWheel.eulerAngles;
+			frontWheelEulerAngles.y=bodyEulerAngles.y+m_wheelRotation;
+			frontWheel.eulerAngles=frontWheelEulerAngles;
+		}
+		//旋转后轮
+		i=rearWheels.Length;
+		while(--i>=0){
+			Transform rearWheel=rearWheels[i];
+			Vector3 rearWheelEulerAngles=rearWheel.eulerAngles;
+			rearWheelEulerAngles.y=bodyEulerAngles.y;//与车身相同
+			rearWheel.eulerAngles=rearWheelEulerAngles;
+		}
+		//重置轮子旋转角
+		if(m_isResetWheeling){
+			m_wheelRotation*=1-resetWheelRate;//速率越大恢复越快
+		}
+		//速度摩擦衰减
+		if(!m_isDriveing){
+			m_speed*=speedDamp;
+		}
+	}
+	
+	/// <summary>
+	/// 驱动，按下方向控制键时每帧调用
+	/// </summary>
+	/// <param name="vNormalized">单位化的移动方向</param>
+	/// <param name="moveDelta">移动速度增量</param>
+	/// <param name="wheelRotateDelta">轮子旋转增量</param>
+	public void Drive(Vector2 vNormalized,float moveDelta=1f,float wheelRotateDelta=1f){
+		m_isDriveing=true;
+		m_isResetWheeling=false;
+		float speedDirection=vNormalized.y>0?1:vNormalized.y<0?-1:0;
+		m_speed=Mathf.Clamp(m_speed+speedDirection*moveDelta,-maxSpeed,maxSpeed);
+
+		float wheelAngleDirection=vNormalized.x>0?1:vNormalized.x<0?-1:0;
+		m_wheelRotation=Mathf.Clamp(m_wheelRotation+wheelAngleDirection*wheelRotateDelta,-maxWheelRotation,maxWheelRotation);
+		if(isResetWheelRotation){
+			m_isResetWheeling=wheelAngleDirection==0;
+		}
+	}
+	
+	/// <summary> 停止驱动 </summary>
+	public void StopDrive(){
+		m_isDriveing=false;
+		m_isResetWheeling=isResetWheelRotation;
+	}
+}
---
+++ Assets/Plugins/Framework/Objs/WheelsControls/WheelDrive.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+#pragma warning disable 0649
+//轮子的外观必须是WheelCollider组件对象的子对象,并且在0索引。
+public class WheelDrive:BaseMonoBehaviour{
+	[System.Serializable]
+	public enum DriveType{
+		RearWheelDrive,//后轮驱动
+		FrontWheelDrive,//前轮驱动
+		AllWheelDrive//所有轮驱动
+	}
+
+	[Tooltip("Maximum steering angle of the wheels.")]
+	public float maxAngle=30f;//车轮的最大转向角。
+
+	[Tooltip("Maximum torque applied to the driving wheels.")]
+	public float maxTorque=1000f;//施加在驱动轮上的最大扭矩。
+
+	[Tooltip("Maximum brake torque applied to the driving wheels.")]
+	public float brakeTorque=30000f;//施加在驱动轮上的最大制动(刹车)扭矩。
+
+	[Tooltip("The vehicle's speed when the physics engine can use different amount of sub-steps (in m/s).")]
+	public float criticalSpeed=5f;//子步进算法的speedThreshold（用于WheelCollider.ConfigureVehicleSubsteps方法）。
+
+	[Tooltip("Simulation sub-steps when the speed is below critical.")]
+	public int stepsBelow=5;//当车辆速度低于 speedThreshold 时，模拟子步骤的数量（用于WheelCollider.ConfigureVehicleSubsteps方法）。
+
+	[Tooltip("Simulation sub-steps when the speed is above critical.")]
+	public int stepsAbove=3;//当车辆速度高于 speedThreshold 时，模拟子步骤的数量（用于WheelCollider.ConfigureVehicleSubsteps方法）。
+
+	[Tooltip("The vehicle's drive type: rear-wheels drive, front-wheels drive or all-wheels drive.")]
+	public DriveType driveType;//车辆的驱动类型：RearWheelDrive（后轮驱动）、FrontWheelDrive（前轮驱动）、AllWheelDrive（所有轮驱动）。
+	
+	[Space,SerializeField] private WheelCollider[] m_frontWheels;//前轮列表
+	
+	[Space,SerializeField] private WheelCollider[] m_rearWheels;//后轮列表
+	
+	private float _steerAngleNormalized=0;
+	private float _motorTorqueNormalized=0;
+	private bool _isBrake;
+
+	protected override void FixedUpdate2(){
+		base.FixedUpdate2();
+		//配置车辆子步进参数
+		//每次进行固定更新时，车辆模拟将该固定增量时间拆分为较小的子步骤，并计算每个较小增量的悬架和轮胎力。然后，汇总所有计算得出的力和扭矩，将它们整合到一起并应用于车身。
+		//利用该函数，您可以自定义在高于和低于速度阈值时模拟将执行的子步骤数。
+		//对于每辆汽车，调用该函数一次即可，因为它实际上是向车辆而不是向某个车轮设置参数。
+		m_frontWheels[0].ConfigureVehicleSubsteps(criticalSpeed,stepsBelow,stepsAbove);
+		//
+		float steerAngle=_steerAngleNormalized*maxAngle;
+		float motorTorque=_motorTorqueNormalized*maxTorque;
+		float brakeTorqueValue=_isBrake?brakeTorque:0;
+		////////////////////////////设置前轮//////////////////////////
+		int i=m_frontWheels.Length;
+		while(--i>=0){
+			WheelCollider wheel=m_frontWheels[i];
+			wheel.steerAngle=steerAngle;//前轮设置转向角
+			if(driveType!=DriveType.RearWheelDrive){
+				wheel.motorTorque=motorTorque;
+			}
+			UpdateWheelSkin(wheel);
+		}
+		////////////////////////////设置后轮//////////////////////////
+		i=m_rearWheels.Length;
+		while(--i>=0){
+			WheelCollider wheel=m_rearWheels[i];
+			wheel.brakeTorque=brakeTorqueValue;//后轮设置刹车扭矩
+			if(driveType!=DriveType.FrontWheelDrive){
+				wheel.motorTorque=motorTorque;
+			}
+			UpdateWheelSkin(wheel);
+		}
+	}
+	
+	/// <summary>更新车轮外观</summary>
+	private void UpdateWheelSkin(WheelCollider wheel){
+		wheel.GetWorldPose(out Vector3 p,out Quaternion q);
+		Transform skinTransform=wheel.transform.GetChild(0);
+		skinTransform.position=p;
+		skinTransform.rotation=q;
+	}
+	
+	/// <summary>设置刹车</summary>
+	public void SetBrake(bool value){
+		_isBrake=value;
+	}
+	
+	/// <summary>设置单位化的转向角度[-1,1]</summary>
+	public void SetSteerAngleNormalized(float value){
+		_steerAngleNormalized=value;
+	}
+	
+	/// <summary>设置单位化的车轮轴电机扭矩[-1,1]</summary>
+	public void SetMotorTorqueNormalized(float value){
+		_motorTorqueNormalized=value;
+	}
+	
+	/// <summary>当前轮轴转速（以每分钟转数为单位）</summary>
+	public float rpm=>m_frontWheels[0].rpm;
+	
+	/// <summary>指示车轮当前是否与某物发生碰撞</summary>
+	public bool isGrounded{
+		get{
+			int i=m_frontWheels.Length;
+			while(--i>=0){
+				if(m_frontWheels[i].isGrounded){
+					return true;
+				}
+			}
+			i=m_rearWheels.Length;
+			while(--i>=0){
+				if(m_rearWheels[i].isGrounded){
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
