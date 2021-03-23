e50149abb0394e582f33addf3e628fbfe1f214df
kingBook <412126604@qq.com>
2021-03-23 14:48:15 +0800

    update 2021-03-23 14:48

--- Assets/Plugins/Framework/Objs/Physics/SimplePendulum.cs
+++ Assets/Plugins/Framework/Objs/Physics/SimplePendulum.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-/**单摆*/
+/// <summary>单摆 </summary>
 public class SimplePendulum:MonoBehaviour{
 	
 	public float gravity=-9.81f;
@@ -9,22 +9,25 @@ public class SimplePendulum:MonoBehaviour{
 
 	private float m_deltaTime;
 	private float m_currentAngle;
-	private float m_len;//摆长
+	
 	private bool m_isPause;
 	private bool m_isInited;
 	private Vector2 m_velocity;
 
-	/**返回单摆当前欧拉角度*/
+	/// <summary> 摆长 </summary>
+	public float length{ get; set; }
+	/// <summary> 返回单摆当前欧拉角度 </summary>
 	public float eulerAngle=>LocalAngleToWorld(m_currentAngle)*Mathf.Rad2Deg;
 	public Vector2 velocity=>m_velocity;
+	/// <summary> 角速度 </summary>
 	public float w { get; private set; }
 
-	private void Init(){
+	public void Init(){
 		m_deltaTime=Time.fixedDeltaTime;
 		
 		Vector2 relative=(Vector2)targetTransform.position-originPosition;
 		
-		m_len=relative.magnitude;
+		length=relative.magnitude;
 		
 		float angle=Mathf.Atan2(relative.y,relative.x);
 		m_currentAngle=WorldAngleToLocal(angle);
@@ -44,23 +47,22 @@ public class SimplePendulum:MonoBehaviour{
 		float l1,l2,l3,l4;
 		{
 			k1=w;
-			l1=(gravity/m_len)*Mathf.Sin(m_currentAngle);
+			l1=(gravity/length)*Mathf.Sin(m_currentAngle);
 			
 			k2=w+m_deltaTime*l1/2f;
-			l2=(gravity/m_len)*Mathf.Sin(m_currentAngle+m_deltaTime*k1/2f);
+			l2=(gravity/length)*Mathf.Sin(m_currentAngle+m_deltaTime*k1/2f);
 			
 			k3=w+m_deltaTime*l2/2f;
-			l3=(gravity/m_len)*Mathf.Sin(m_currentAngle+m_deltaTime*k2/2f);
+			l3=(gravity/length)*Mathf.Sin(m_currentAngle+m_deltaTime*k2/2f);
 			
 			k4=w+m_deltaTime*l3;
-			l4=(gravity/m_len)*Mathf.Sin(m_currentAngle*m_deltaTime*k3);
+			l4=(gravity/length)*Mathf.Sin(m_currentAngle*m_deltaTime*k3);
 			
 			m_currentAngle+=m_deltaTime*(k1+2f*k2+2f*k3+k4)/(6f/*2f*Math.PI*/);
 			w+=m_deltaTime*(l1+2f*l2+2f*l3+l4)/(6f/*2f*Math.PI*/);
 		}
-
-		float newX=originPosition.x+Mathf.Sin(m_currentAngle)*m_len;
-		float newY=originPosition.y-Mathf.Cos(m_currentAngle)*m_len;
+		float newX=originPosition.x+Mathf.Sin(m_currentAngle)*length;
+		float newY=originPosition.y-Mathf.Cos(m_currentAngle)*length;
 
 		Vector3 targetPos=targetTransform.position;
 
@@ -82,6 +84,15 @@ public class SimplePendulum:MonoBehaviour{
 
 	public void SetPause(bool value){
 		m_isPause=value;
+	}
+
+	/// <summary>
+	/// 立即反向运动（用于摆动到某个角度时立即反向摆动）
+	/// </summary>
+	/// <param name="damping">区间[0,1]，角速度的阻尼</param>
+	public void Reverse(float damping){
+		damping=Mathf.Clamp01(damping);
+		w=-w*0.5f;
 	}
 
 	private void OnDisable(){
