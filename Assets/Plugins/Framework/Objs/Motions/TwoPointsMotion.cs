1f121b8d4e817189344070f22974550ce26092ac
kingBook <412126604@qq.com>
2021-04-09 18:00:43 +0800

    update 2021-04-09 18:00

--- Assets/Plugins/Framework/Objs/Motions/TwoPointsMotion.cs
+++ Assets/Plugins/Framework/Objs/Motions/TwoPointsMotion.cs
@@ -7,82 +7,107 @@ using System.Collections;
 public class TwoPointsMotion : MonoBehaviour {
 
     [Tooltip("运动的起始点，None 时以当前位置为起始点（只读取在 Start() 时的位置）")] public Transform startTransform;
-    [Tooltip("运动的目标点（只读取在 Start() 时的位置）")] public Transform targetTransform;
-    [Tooltip("true 时，首次运动时是否朝目标点；false 时，首次运动时是否朝起始点")] public bool isFirstToTarget = true;
-    [Tooltip("运动速度。")] public float speed = 1f;
-    [Tooltip("到达一个点时，等待的时间(秒)"), Range(0f, 10f)] public float waitTime;
-    [Tooltip("当到达一个点时，需要同步转向的其他 TwoPointsMotion")] public TwoPointsMotion[] syncReversesObjects;
+	[Tooltip("运动的目标点（只读取在 Start() 时的位置）")] public Transform targetTransform;
+	[Tooltip("true 时，首次运动时是否朝目标点；false 时，首次运动时是否朝起始点")] public bool isFirstToTarget = true;
+	[Tooltip("运动速度。")] public float speed = 1f;
+	[Tooltip("到达一个点时，等待的时间(秒)"), Range(0f, 10f)] public float waitTime;
+	[Tooltip("当到达一个点时，需要同步转向的其他 TwoPointsMotion")] public TwoPointsMotion[] syncReversesObjects;
 
-    private Vector3 m_positionRecord;
-    private Vector3 m_targetRecord;
-    private Vector3 m_currentGotoTarget;
-    private bool m_isWaiting;
+	private Vector3 m_positionRecord;
+	private Vector3 m_targetRecord;
+	private Vector3 m_currentGotoTarget;
+	private bool m_isWaiting;
+	private Vector3 m_lastPosition;
 
-    /// <summary>
-    /// 设置反转运动
-    /// </summary>
-    public void SetReverseGotoTarget () {
-        m_currentGotoTarget = (m_currentGotoTarget == m_positionRecord) ? m_targetRecord : m_positionRecord;
-        //如果在等待中，则停止计时
-        StopWaitTimer();
-    }
 
-    private bool GotoTarget (Vector3 current, Vector3 target, float maxDistanceDelta) {
-        transform.position = Vector3.MoveTowards(current, target, maxDistanceDelta);
-        float distance = Vector3.Distance(current, target);
-        return distance <= 0.01f;
-    }
+	/// <summary>
+	/// 移动事件，回调格式：void onMoveing (Vector3 velocity)
+	/// </summary>
+	public event System.Action<Vector3> onMoveingEvent;
 
-    private void ReverseSyncObjects () {
-        if (syncReversesObjects != null) {
-            for (int i = 0, len = syncReversesObjects.Length; i < len; i++) {
-                syncReversesObjects[i].SetReverseGotoTarget();
-            }
-        }
-    }
+	public Vector3 velocity { get; private set; }
 
-    private void StartWaitTimer () {
-        Invoke(nameof(OnWaitTimeEnd), waitTime);
-    }
+	/// <summary>
+	/// 设置反转运动
+	/// </summary>
+	public void SetReverseGotoTarget () {
+		m_currentGotoTarget = (m_currentGotoTarget == m_positionRecord) ? m_targetRecord : m_positionRecord;
+		//如果在等待中，则停止计时
+		StopWaitTimer();
+	}
 
-    private void StopWaitTimer () {
-        CancelInvoke(nameof(OnWaitTimeEnd));
-    }
+	private bool GotoTarget (Vector3 current, Vector3 target, float maxDistanceDelta) {
+		transform.position = Vector3.MoveTowards(current, target, maxDistanceDelta);
+		float distance = Vector3.Distance(current, target);
+		return distance <= 0.01f;
+	}
 
-    private void OnWaitTimeEnd () {
-        SetReverseGotoTarget();
-        ReverseSyncObjects();
-    }
+	private void ReverseSyncObjects () {
+		if (syncReversesObjects != null) {
+			for (int i = 0, len = syncReversesObjects.Length; i < len; i++) {
+				syncReversesObjects[i].SetReverseGotoTarget();
+			}
+		}
+	}
 
-    private void Start () {
-        //记录起始点
-        if (startTransform) {
-            m_positionRecord = startTransform.position;
-        } else {
-            m_positionRecord = transform.position;
-        }
+	private void StartWaitTimer () {
+		Invoke(nameof(OnWaitTimeEnd), waitTime);
+	}
 
-        //记录目标点
-        m_targetRecord = targetTransform.position;
+	private void StopWaitTimer () {
+		CancelInvoke(nameof(OnWaitTimeEnd));
+	}
 
-        //设置首次运动的目标
-        if (isFirstToTarget) {
-            m_currentGotoTarget = m_targetRecord;
-        } else {
-            m_currentGotoTarget = m_positionRecord;
-        }
-    }
+	private void OnWaitTimeEnd () {
+		SetReverseGotoTarget();
+		ReverseSyncObjects();
+	}
 
-    private void FixedUpdate () {
-        if (m_isWaiting) return;
+	private void Start () {
+		//记录起始点
+		if (startTransform) {
+			m_positionRecord = startTransform.position;
+		} else {
+			m_positionRecord = transform.position;
+		}
 
-        if (GotoTarget(transform.position, m_currentGotoTarget, Time.deltaTime * speed)) {
-            if (waitTime > 0) {
-                StartWaitTimer();
-            } else {
-                SetReverseGotoTarget();
-                ReverseSyncObjects();
-            }
-        }
-    }
+		//记录目标点
+		m_targetRecord = targetTransform.position;
+
+		//设置首次运动的目标
+		if (isFirstToTarget) {
+			m_currentGotoTarget = m_targetRecord;
+		} else {
+			m_currentGotoTarget = m_positionRecord;
+		}
+
+		//记录初始位置
+		m_lastPosition = transform.position;
+	}
+
+	private void FixedUpdate () {
+		if (m_isWaiting) return;
+
+		Vector3 position = transform.position;
+
+		if (GotoTarget(position, m_currentGotoTarget, Time.deltaTime * speed)) {
+			if (waitTime > 0) {
+				StartWaitTimer();
+			} else {
+				SetReverseGotoTarget();
+				ReverseSyncObjects();
+			}
+		}
+
+		//计算移动速度
+		velocity = position - m_lastPosition;
+
+		//调用移动事件
+		if (velocity.magnitude > 0f) {
+			onMoveingEvent?.Invoke(velocity);
+		}
+
+		//记录上一次位置，用于计算移动速度向量
+		m_lastPosition = position;
+	}
 }
