e27aad8d3dfcd750d82a63326486631c97c239e3
kingBook <412126604@qq.com>
2021-04-22 17:54:55 +0800

    update 2021-04-22 17:54

---
+++ Assets/Plugins/Framework/Objs/UIControls/LanguageSwapTextString.cs
@@ -0,0 +1,50 @@
+﻿#pragma warning disable 0649
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 根据语言交换文本框的文本
+/// </summary>
+public class LanguageSwapTextString : MonoBehaviour {
+
+    public string stringEN;
+    public string stringCN;
+    public Text text;
+
+    private void OnChangeLanguage (App.Language language) {
+        SwapStringToLanguage(language);
+    }
+
+#if UNITY_EDITOR
+    private void Reset () {
+        if (!text) {
+            text = GetComponent<Text>();
+        }
+    }
+#endif
+
+    private void Awake () {
+        if (App.instance != null) {
+            SwapStringToLanguage(App.instance.language);
+        }
+    }
+
+    private void Start () {
+        SwapStringToLanguage(App.instance.language);
+        App.instance.onChangeLanguageEvent += OnChangeLanguage;
+    }
+
+    private void SwapStringToLanguage (App.Language language) {
+        if (language == App.Language.EN) {
+            if (text != null) text.text = stringEN;
+        } else if (language == App.Language.CN) {
+            if (text != null) text.text = stringCN;
+        }
+    }
+
+    private void OnDestroy () {
+        App.instance.onChangeLanguageEvent -= OnChangeLanguage;
+    }
+}
