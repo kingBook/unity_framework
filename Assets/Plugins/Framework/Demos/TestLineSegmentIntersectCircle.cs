0bc692e5941ec612aff83856b2dd2b0cb9511b2c
kingBook <412126604@qq.com>
2021-04-30 18:49:28 +0800

    update 2021-04-30 18:49

---
+++ Assets/Plugins/Framework/Demos/TestLineSegmentIntersectCircle.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLineSegmentIntersectCircle : MonoBehaviour {
+
+    public Transform lineStart;
+    public Transform lineEnd;
+
+    public Transform circleCenter;
+    public float radius = 1;
+
+    /// <summary> 交点1 </summary>
+    private Vector2 m_intersection1;
+    private Vector2 m_intersection2;
+
+
+    private void DrawGizmosCircle (Vector3 circleCenter, float radius, float thetaStep = 0.1f) {
+        thetaStep = Mathf.Max(thetaStep, 1e-4f);
+        Vector3 firstPoint = Vector3.zero;
+        Vector3 lastPoint = Vector3.zero;
+        Vector3 currentPoint = Vector3.zero;
+        for (float theta = 0; theta < 2 * Mathf.PI; theta += thetaStep) {
+            float x = radius * Mathf.Cos(theta);
+            float y = radius * Mathf.Sin(theta);
+            if (theta == 0) {
+                firstPoint.Set(x, y, 0);
+                lastPoint.Set(x, y, 0);
+            } else {
+                currentPoint.Set(x, y, 0);
+                Gizmos.DrawLine(lastPoint + circleCenter, currentPoint + circleCenter);
+                lastPoint.Set(x, y, 0);
+            }
+        }
+        Gizmos.DrawLine(firstPoint + circleCenter, lastPoint + circleCenter); // 绘制最后一条线段
+    }
+
+    private void Update () {
+        Vector2 direction = lineEnd.position - lineStart.position;
+
+        //GeomUtil.Ray2DIntersectCircle(lineStart.position, direction, circleCenter.position, radius, out m_intersection1, out m_intersection2);
+
+        int intersectionCount = GeomUtil.LineSegmentIntersectCircle(lineStart.position, lineEnd.position, circleCenter.position, radius, out m_intersection1, out m_intersection2);
+        Debug.Log("线段与圆交点的数量："+intersectionCount);
+    }
+
+    private void OnDrawGizmos () {
+        if (!Application.isPlaying) return;
+        // 画圆
+        DrawGizmosCircle(circleCenter.position, radius);
+        // 画直线
+        Gizmos.DrawLine(lineStart.position, lineEnd.position);
+        // 画交点
+        Gizmos.DrawWireSphere(m_intersection1, 0.2f);
+        Gizmos.DrawWireSphere(m_intersection2, 0.2f);
+    }
+}
---
+++ Assets/Plugins/Framework/Utils/GeomUtil.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class GeomUtil {
+
+    /// <summary>
+    /// 计算线段与圆的交点
+    /// </summary>
+    /// <param name="lineStart"> 线段的起始点 </param>
+    /// <param name="lineEnd"> 线段的终点 </param>
+    /// <param name="circleCenter"> 圆心 </param>
+    /// <param name="radius"> 半径 </param>
+    /// <param name="intersection1"> 输出的交点1 </param>
+    /// <param name="intersection2"> 输出的交点2 </param>
+    /// <returns> 返回交点的数量，0：不相交；1：有1个交点；2：有2个交点 </returns>
+    public static int LineSegmentIntersectCircle (Vector2 lineStart, Vector2 lineEnd, Vector2 circleCenter, float radius, out Vector2 intersection1, out Vector2 intersection2) {
+        int intersectCount = 0;
+        bool intersect = Ray2DIntersectCircle(lineStart, lineEnd - lineStart, circleCenter, radius, out intersection1, out intersection2);
+        if (intersect) {
+            float lineSegmentLength = Vector2.Distance(lineStart, lineEnd);
+            float d1 = Vector2.Distance(lineStart, intersection1 - lineStart);
+            float d2 = Vector2.Distance(lineStart, intersection2 - lineStart);
+            if (d1 <= lineSegmentLength) {
+                intersectCount++;
+            }
+            if (d2 <= lineSegmentLength) {
+                intersectCount++;
+            }
+        }
+        return intersectCount;
+    }
+
+    /// <summary>
+    /// 计算射线与圆的交点
+    /// </summary>
+    /// <param name="ray"> 2D射线 </param>
+    /// <param name="circleCenter"> 圆心 </param>
+    /// <param name="radius"> 半径 </param>
+    /// <param name="intersection1"> 输出的交点1 </param>
+    /// <param name="intersection2"> 输出的交点2 </param>
+    /// <returns> 是否相交 </returns>
+    public static bool Ray2DIntersectCircle (Ray2D ray, Vector2 circleCenter, float radius, out Vector2 intersection1, out Vector2 intersection2) {
+        intersection1 = Vector2.zero;
+        intersection2 = Vector2.zero;
+
+        Vector2 e = circleCenter - ray.origin;
+        float a = Vector2.Dot(ray.direction, e);
+        float fSqr = radius * radius - e.sqrMagnitude + a * a;
+        if (fSqr >= 0) {
+            float f = Mathf.Sqrt(fSqr);
+            float t = a - f;
+            if (t >= 0) {
+                intersection1 = ray.origin + ray.direction * t;
+                intersection2 = ray.origin + ray.direction * (t + 2f * f);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// 计算射线与圆的交点
+    /// </summary>
+    /// <param name="origin"> 2D射线的原点 </param>
+    /// <param name="direction"> 2D射线的方向 </param>
+    /// <param name="circleCenter"> 圆心 </param>
+    /// <param name="radius"> 半径 </param>
+    /// <param name="intersection1"> 输出的交点1 </param>
+    /// <param name="intersection2"> 输出的交点2 </param>
+    /// <returns> 是否相交 </returns>
+    public static bool Ray2DIntersectCircle (Vector2 origin, Vector2 direction, Vector2 circleCenter, float radius, out Vector2 intersection1, out Vector2 intersection2) {
+        Ray2D ray = new Ray2D(origin, direction);
+        return Ray2DIntersectCircle(ray, circleCenter, radius, out intersection1, out intersection2);
+    }
+}
