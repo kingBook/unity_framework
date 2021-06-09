fcb46f02f83f3ba72c782af3a10ff7aa6bf48d83
kingBook <412126604@qq.com>
2021-06-09 18:01:52 +0800

    update 2021-06-09 18:01

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
 /// <summary>
 /// 游戏类
 /// <br>管理游戏全局变量、本地数据、场景切换。</br>
@@ -6,11 +8,13 @@
 /// </summary>
 public sealed class Game : BaseGame {
 
+    public int levelNumber { get; private set; }
+
     private void Start () {
 		// 非调试时，才加载其它场景
         if (!App.instance.isDebug) {
             //GotoTitleScene();
-            GotoLevelScene();
+            GotoLevelScene(1);
         }
     }
 
@@ -18,8 +22,22 @@ public sealed class Game : BaseGame {
         App.instance.sceneLoader.Load("Scenes/Title");
     }
 
-    public void GotoLevelScene () {
+    public void GotoLevelScene (int levelNum) {
+        levelNumber = levelNum;
+
         App.instance.sceneLoader.LoadAsync("Scenes/Level_0");
+    }
+
+    /// <summary> 重新开始当前关 </summary>
+    public void RestartCurrentLevel (Scene sceneUnload) {
+        SceneManager.UnloadSceneAsync(sceneUnload);
+        GotoLevelScene(levelNumber);
+    }
+
+    /// <summary> 下一关 </summary>
+    public void NextLevel (Scene sceneUnload) {
+        SceneManager.UnloadSceneAsync(sceneUnload);
+        GotoLevelScene(levelNumber + 1);
     }
 
     private void OnDestroy () {
