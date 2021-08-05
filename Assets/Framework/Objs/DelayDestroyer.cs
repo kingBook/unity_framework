d5f2754e1423ac9bbeafdf9369a9fdc483a115fd
kingBook <412126604@qq.com>
2021-08-05 17:50:47 +0800

    update 2021-08-05 17:50

---
+++ Assets/Framework/Objs/DelayDestroyer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// 延时销毁器
+/// </summary>
+public class DelayDestroyer : MonoBehaviour {
+
+    public float time = 5;
+
+    private bool m_timerStarting;
+
+    private void DestroySelf () {
+        Destroy(gameObject);
+    }
+
+    private void Update () {
+        if (m_timerStarting) return;
+
+        m_timerStarting = true;
+        Invoke(nameof(DestroySelf), time);
+    }
+}
