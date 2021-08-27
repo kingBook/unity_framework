5bfeba6d88599158cc212167a4a2c054224bf7a7
kingBook <412126604@qq.com>
2021-08-27 18:22:01 +0800

    update 2021-08-27 18:21

---
+++ Assets/Framework/Objs/Particles/ParticleSystemSimulationTime.cs
@@ -0,0 +1,24 @@
+﻿#pragma warning disable 0649
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 指定一个时间，让粒子系统从该时间的状态开始模拟
+/// </summary>
+public class ParticleSystemSimulationTime : MonoBehaviour {
+
+    [Tooltip("模拟粒子的时间")] public float time = 5;
+
+    private ParticleSystem m_particleSystem;
+
+    private void Awake () {
+        m_particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    private void Start () {
+        m_particleSystem.Simulate(time, true, true);
+        m_particleSystem.Play();
+    }
+}
---
+++ Assets/Framework/Objs/UIControls/RenderTextureUISetter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 将创建一个 RenderTexture，并将其设置 Camera.targetTexture 和 RawImage.texture
+/// </summary>
+public class RenderTextureUISetter : MonoBehaviour {
+
+    public RawImage rawImage;
+    public Camera cam;
+
+    [Space]
+    public Vector2Int renderTextureSize = new Vector2Int(512, 512);
+    public int depthBuffer = 16;
+    public RenderTextureFormat renderTextureFormat = RenderTextureFormat.ARGB32;
+
+    private RenderTexture m_renderTexture;
+
+    private void OnEnable () {
+        m_renderTexture = RenderTexture.GetTemporary(renderTextureSize.x, renderTextureSize.y, depthBuffer, renderTextureFormat);
+
+        rawImage.texture = m_renderTexture;
+        cam.targetTexture = m_renderTexture;
+        //必须为 CameraClearFlags.SolidColor或CameraClearFlags.Depth，CameraClearFlags.Nothing 时会不显示
+        cam.clearFlags = CameraClearFlags.SolidColor;
+
+        //CameraClearFlags.SolidColor时会有背景色，需要设置背景色透明
+        Color color = cam.backgroundColor;
+        color.a = 0f;
+        cam.backgroundColor = color;
+    }
+
+    private void Update () {
+        if (!m_renderTexture.IsCreated()) {
+            m_renderTexture.Create();
+        }
+    }
+
+    private void OnDisable () {
+        m_renderTexture.Release();
+    }
+}
