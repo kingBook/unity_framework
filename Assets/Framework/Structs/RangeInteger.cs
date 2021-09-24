a57d07bc2a2a26d7ec0da8180301c450270853a5
kingBook <412126604@qq.com>
2021-09-24 18:37:25 +0800

    update 2021-09-24 18:37

---
+++ Assets/Framework/Structs/RangeInteger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class RangeInteger {
+
+    public int min;
+    public int max;
+
+    public RangeInteger (int minValue, int maxValue) {
+        min = minValue;
+        max = maxValue;
+    }
+
+    public float Lerp (float t) {
+        return Mathf.Lerp(min, max, t);
+    }
+}
---
+++ Assets/Framework/Structs/RangeVector2.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct RangeVector2 {
+    public Vector2 min;
+    public Vector2 max;
+    public RangeVector2 (Vector2 minValue, Vector2 maxValue) {
+        min = minValue;
+        max = maxValue;
+    }
+}
