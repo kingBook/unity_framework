d36ef765f5e64c959c63b3f66a2c5993418f5c84
kingBook <412126604@qq.com>
2021-09-26 09:17:50 +0800

    update 2021-09-26 09:17

---
+++ Assets/Framework/Editor/Objs/RangeIntegerDrawer.cs
@@ -0,0 +1,37 @@
+﻿#pragma warning disable 0649
+
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(RangeInteger))]
+public class RangeIntegerDrawer : PropertyDrawer {
+
+    public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
+        EditorGUI.BeginProperty(position, label, property);
+        {
+            float x, w;
+            int elementCount = 3;
+            for (int i = 0; i < elementCount; i++) {
+                w = position.width / elementCount;
+                x = position.x + w * i;
+                switch (i) {
+                    case 0:
+                        EditorGUIUtility.labelWidth = 25;
+                        EditorGUI.LabelField(new Rect(x, position.y, w, position.height), property.displayName);
+                        break;
+                    case 1:
+                        EditorGUIUtility.labelWidth = 25;
+                        EditorGUI.PropertyField(new Rect(x, position.y, w, position.height), property.FindPropertyRelative("min"));
+                        break;
+                    case 2:
+                        EditorGUIUtility.labelWidth = 25;
+                        EditorGUI.PropertyField(new Rect(x, position.y, w, position.height), property.FindPropertyRelative("max"));
+                        break;
+                }
+            }
+        }
+        EditorGUI.EndProperty();
+        EditorGUIUtility.labelWidth = 0; // 将其设置为 0 会重置为默认值
+    }
+
+}
---
+++ Assets/Framework/Editor/Objs/RangeVector3Drawer.cs
@@ -0,0 +1,35 @@
+﻿#pragma warning disable 0649
+
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(RangeVector3))]
+public class RangeVector3Drawer : PropertyDrawer {
+
+    public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
+        var minProperty = property.FindPropertyRelative("min");
+        var maxProperty = property.FindPropertyRelative("max");
+        EditorGUI.BeginProperty(position, label, property);
+        {
+            float x, w;
+            int elementCount = 3;
+            for (int i = 0; i < elementCount; i++) {
+                w = position.width / elementCount;
+                x = position.x + w * i;
+                switch (i) {
+                    case 0:
+                        EditorGUI.LabelField(new Rect(x, position.y, w, position.height), property.displayName);
+                        break;
+                    case 1:
+                        EditorGUI.PropertyField(new Rect(x, position.y, w, position.height), minProperty, GUIContent.none);
+                        break;
+                    case 2:
+                        EditorGUI.PropertyField(new Rect(x, position.y, w, position.height), maxProperty, GUIContent.none);
+                        break;
+                }
+            }
+        }
+        EditorGUI.EndProperty();
+    }
+
+}
