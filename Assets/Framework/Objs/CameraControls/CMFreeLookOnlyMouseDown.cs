469169ae0bfe8de06cafd9a7ca5021fd5da0b64d
kingBook <412126604@qq.com>
2022-01-23 16:32:08 +0800

    update 2022-01-23 16:32

--- Assets/Framework/Objs/CameraControls/CMFreeLookOnlyMouseDown.cs
+++ Assets/Framework/Objs/CameraControls/CMFreeLookOnlyMouseDown.cs
@@ -1,7 +1,7 @@
 ﻿using Cinemachine;
 using System.Collections;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(CinemachineFreeLook))]
 public class CMFreeLookOnlyMouseDown : MonoBehaviour {
@@ -11,7 +11,10 @@ public class CMFreeLookOnlyMouseDown : MonoBehaviour {
     /// <para> * 因为 Touch.deltaPosition 是基于屏幕坐标计算的，和 Input.GetAxis("Mouse X") 是不一样的，直接使用 Touch.deltaPosition 会出现 PC 平台与移动平台不兼容 </para>
     /// <para> * 移动平台如果 使用 Input.GetAxis("Mouse X") ,会出现每次触摸开始时都会出现很大的值，即使未滑动</para>
     /// </summary>
-    [SerializeField, Tooltip("触摸的敏感度（仅用于移动平台）")] private float m_touchSensitivity = 0.1f; 
+    [SerializeField, Tooltip("触摸的敏感度（仅用于移动平台）")] private float m_touchSensitivity = 0.01f;
+    [SerializeField] private bool m_ignorePointerOverUI = true;
+
+
     private CinemachineFreeLook m_cinemachineFreeLook;
 
     private void Awake () {
@@ -25,14 +28,24 @@ public class CMFreeLookOnlyMouseDown : MonoBehaviour {
         if (Input.GetMouseButton(0)) {
             if (!Input.touchSupported) {
                 // PC
-                m_cinemachineFreeLook.m_XAxis.m_InputAxisValue = Input.GetAxis("Mouse X");
-                m_cinemachineFreeLook.m_YAxis.m_InputAxisValue = Input.GetAxis("Mouse Y");
+                bool isPointerOverUI = EventSystem.current && EventSystem.current.IsPointerOverGameObject();
+                if (m_ignorePointerOverUI && isPointerOverUI) {
+                    // 触摸 UI时，忽略
+                } else {
+                    m_cinemachineFreeLook.m_XAxis.m_InputAxisValue = Input.GetAxis("Mouse X");
+                    m_cinemachineFreeLook.m_YAxis.m_InputAxisValue = Input.GetAxis("Mouse Y");
+                }
             } else {
                 // 移动平台
                 if (Input.touchCount > 0) {
                     Touch touch = Input.GetTouch(0);
-                    m_cinemachineFreeLook.m_XAxis.m_InputAxisValue = touch.deltaPosition.x * m_touchSensitivity;
-                    m_cinemachineFreeLook.m_YAxis.m_InputAxisValue = touch.deltaPosition.y * m_touchSensitivity;
+                    bool isPointerOverUI = EventSystem.current && EventSystem.current.IsPointerOverGameObject(touch.fingerId);
+                    if (m_ignorePointerOverUI && isPointerOverUI) {
+                        // 触摸 UI时，忽略
+                    } else {
+                        m_cinemachineFreeLook.m_XAxis.m_InputAxisValue = touch.deltaPosition.x * m_touchSensitivity;
+                        m_cinemachineFreeLook.m_YAxis.m_InputAxisValue = touch.deltaPosition.y * m_touchSensitivity;
+                    }
                 }
             }
         } else {
