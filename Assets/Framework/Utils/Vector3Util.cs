da3dd3f43e683830eb0d9d7839b1da803d9ff66f
kingBook <412126604@qq.com>
2022-02-22 15:28:26 +0800

    update 2022-02-22 15:28

---
+++ Assets/Framework/Utils/Vector3Util.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class Vector3Util {
+
+    /// <summary> 计算两个向量在 XZ 平面上的距离 </summary>
+    public static float DistanceXZ(Vector3 a,Vector3 b) {
+        a.y = b.y = 0f;
+        return Vector3.Distance(a,b);
+    }
+
+    /// <summary> 计算两个向量在 XY 平面上的距离 </summary>
+    public static float DistanceXY (Vector3 a, Vector3 b) {
+        a.z = b.z = 0f;
+        return Vector3.Distance(a, b);
+    }
+}
