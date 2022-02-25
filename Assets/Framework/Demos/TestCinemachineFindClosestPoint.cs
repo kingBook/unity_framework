5d5dcfaa2a2d252321c57c424039c8b3a95492f1
kingBook <412126604@qq.com>
2022-02-25 16:01:45 +0800

    update 2022-02-25 16:01

---
+++ Assets/Framework/Demos/TestCinemachineFindClosestPoint.cs
@@ -0,0 +1,21 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCinemachineFindClosestPoint : MonoBehaviour {
+
+    public Transform player;
+    public Transform redPoint;
+    public CinemachineSmoothPath smoothPath;
+
+    void Update () {
+        // 查找 CinemachinePath 路径上距离玩家最近的点
+        float playerPosUnits = smoothPath.FindClosestPoint(player.position, 0, -1, 0);
+
+        Debug.Log($"playerPosUnits:{playerPosUnits}");
+        Vector3 playerPosOnPath = smoothPath.EvaluatePosition(playerPosUnits); // 玩家在路径上的世界坐标
+
+        redPoint.position = playerPosOnPath;
+    }
+}
