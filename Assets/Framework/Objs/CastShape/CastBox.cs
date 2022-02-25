35366606a9c9bbb98f86d1de57f4fb64f4aa150a
kingBook <412126604@qq.com>
2022-02-25 17:53:49 +0800

    update 2022-02-25 17:53

---
+++ Assets/Framework/Objs/CastShape/CastBox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CastBox : CastShapeBase {
+
+    public Vector3 center;
+    public Vector3 size = Vector3.one;
+
+#if UNITY_EDITOR
+    protected override void Reset () {
+        center = Vector3.zero;
+        size = Vector3.one;
+    }
+
+    protected override void OnDrawGizmosSelected () {
+        Color gizmosColorRecord = Gizmos.color;
+        Gizmos.color = m_gizomsColor;
+
+
+        Matrix4x4 localToWorldMatrix = transform.localToWorldMatrix;
+
+        Gizmos.DrawWireCube(transform.position, localToWorldMatrix * size);
+
+        Gizmos.color = gizmosColorRecord;
+    }
+#endif
+}
---
+++ Assets/Framework/Objs/CastShape/CastShapeBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public abstract class CastShapeBase : MonoBehaviour {
+
+
+#if UNITY_EDITOR
+    protected readonly Color m_gizomsColor = Color.cyan;
+
+    protected virtual void Reset () {
+
+    }
+
+    protected virtual void OnDrawGizmosSelected () {
+
+    }
+#endif
+
+
+}
