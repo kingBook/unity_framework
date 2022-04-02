cd0175bf8069e9bf511ef93fbc2a68b07f524741
kingBook <412126604@qq.com>
2022-04-02 18:12:05 +0800

    update 2022-04-02 18:12

---
+++ Assets/Framework/Objs/UIControls/CanvasScreenModeMatchModeAdapter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 在 OnEnable 时重新设置 CanvasScaler 的 matchWidthOrHeight ，OnDisable 时再还原
+/// </summary>
+public class CanvasScreenModeMatchModeAdapter : MonoBehaviour {
+
+    public float matchWidthOrHeight = 0f;
+
+    private CanvasScaler m_canvasScaler;
+    private float m_matchWidthOrHeightRecord;
+
+    private void Awake() {
+        m_canvasScaler = GetComponentInParent<CanvasScaler>();
+    }
+
+    private void OnEnable() {
+        m_matchWidthOrHeightRecord = m_canvasScaler.matchWidthOrHeight;
+        m_canvasScaler.matchWidthOrHeight = matchWidthOrHeight;
+    }
+
+    private void OnDisable() {
+        if(this && m_canvasScaler) {
+            m_canvasScaler.matchWidthOrHeight = m_matchWidthOrHeightRecord;
+        }
+    }
+}
