862522a5af0fc69e7212b773bf76128494fe257c
kingBook <412126604@qq.com>
2022-05-16 10:32:26 +0800

    update 2022-05-06 10:32

--- Packages/External Assets/Particlecollection_Free samples/Plugins/Effect/Editor/EffectControllerInspector.cs
+++ Packages/External Assets/Particlecollection_Free samples/Plugins/Effect/Editor/EffectControllerInspector.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using UnityEditor;
 using System.Collections;
 using System.Text.RegularExpressions;
@@ -137,3 +138,4 @@ public class EffectControllerInspector : Editor
 		EditorGUILayout.EndVertical ();
 	}
 }
+#endif
