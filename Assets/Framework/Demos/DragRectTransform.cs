eea82af8d0c879703a22fe0ee6fcf67e0ac19532
kingBook <412126604@qq.com>
2022-06-27 23:26:58 +0800

    update 2022-06-27 23:26

--- Assets/Framework/Demos/DragRectTransform.cs
+++ Assets/Framework/Demos/DragRectTransform.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#pragma warning disable 0414
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -7,32 +8,13 @@ using UnityEngine.UI;
 /// <summary>
 /// 拖 RectTransform 示例
 /// </summary>
-public class DragRectTransform : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+public class DragRectTransform : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler {
 
     private Canvas m_canvas;
     private RectTransform m_rectTransform;
     private bool m_isPointerDown;
     private Vector2 m_lastMousePos;
     private Vector2 m_positionOnPointerDown;
-
-
-    void IPointerDownHandler.OnPointerDown(PointerEventData eventData) {
-        m_lastMousePos = Input.mousePosition;
-        m_isPointerDown = true;
-        m_positionOnPointerDown = m_rectTransform.position;
-    }
-
-    void IPointerUpHandler.OnPointerUp(PointerEventData eventData) {
-        m_isPointerDown = false;
-
-        if (Vector2.Distance(m_rectTransform.position, m_positionOnPointerDown) <= 1.0f) {
-            OnPointerClick(eventData); // 按下并释放后，如果未发生移动，则视为点击
-        }
-    }
-
-    private void OnPointerClick(PointerEventData eventData) {
-        Debug.Log("OnPointerClick");
-    }
 
     #region Util
     private static void Move(RectTransform rectTransform, Vector2 velocityOnScreen, Canvas canvas, bool isLimitToScreen) {
@@ -51,7 +33,7 @@ public class DragRectTransform : MonoBehaviour, IPointerDownHandler, IPointerUpH
         //Vector2 sizeOnScreen = rectTransform.sizeDelta * canvas.scaleFactor; // 如果 rectTransform.Anchors 的四个角点在同一个点时，也可以使用此行代码
 
         Vector2 min = sizeOnScreen * rectTransform.pivot; // rectTransform 可移动的最小屏幕坐标值
-        Vector2 max = new Vector2(Screen.width, Screen.height) - sizeOnScreen * (Vector2.one-rectTransform.pivot); // rectTransform 可移动的最大屏幕坐标值
+        Vector2 max = new Vector2(Screen.width, Screen.height) - sizeOnScreen * (Vector2.one - rectTransform.pivot); // rectTransform 可移动的最大屏幕坐标值
         // 如果 rectTransform.pivot == (0.5,0.5) 时，也可以使用以下代码
         //Vector2 min = sizeOnScreen * 0.5f; 
         //Vector2 max = new Vector2(Screen.width, Screen.height) - sizeOnScreen * 0.5f;
@@ -65,12 +47,33 @@ public class DragRectTransform : MonoBehaviour, IPointerDownHandler, IPointerUpH
         m_canvas = m_rectTransform.root.GetComponent<Canvas>();
     }
 
-    private void Update() {
-        if (m_isPointerDown) {
-            Vector2 mousePos = Input.mousePosition;
-            Vector2 velocity = mousePos - m_lastMousePos;
-            Move(m_rectTransform, velocity, m_canvas, true);
-            m_lastMousePos = mousePos;
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData) {
+        m_lastMousePos = Input.mousePosition;
+        m_isPointerDown = true;
+        m_positionOnPointerDown = m_rectTransform.position;
+    }
+
+    void IPointerUpHandler.OnPointerUp(PointerEventData eventData) {
+        m_isPointerDown = false;
+        if (Vector2.Distance(m_rectTransform.position, m_positionOnPointerDown) <= 1.0f) {
+            OnPointerClick(eventData); // 按下并释放后，如果未发生移动，则视为点击
         }
     }
+
+    private void OnPointerClick(PointerEventData eventData) {
+        Debug.Log("OnPointerClick");
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData) {
+        Move(m_rectTransform, eventData.delta, m_canvas, true);
+    }
+    // 或
+    //private void Update() {
+    //    if (m_isPointerDown) {
+    //        Vector2 mousePos = Input.mousePosition;
+    //        Vector2 velocity = mousePos - m_lastMousePos;
+    //        Move(m_rectTransform, velocity, m_canvas, true);
+    //        m_lastMousePos = mousePos;
+    //    }
+    //}
 }
