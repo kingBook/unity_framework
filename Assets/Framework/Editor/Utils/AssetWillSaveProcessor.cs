68519a13ec66cdf613dc97aee72f646055f46f3f
kingBook <412126604@qq.com>
2022-07-03 03:33:17 +0800

    update 2022-07-03 03:33

---
+++ Assets/Framework/Editor/Utils/AssetWillSaveProcessor.cs
@@ -0,0 +1,25 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class AssetWillSaveProcessor : UnityEditor.AssetModificationProcessor {
+
+    private const string SCENE_EXTENSION = ".unity";
+
+    /// <summary>
+    /// 将要保存某个场景前事件，回调函数格式：<code> void OnWillSaveSceneHandler(Scene scene) </code>
+    /// </summary>
+    public static event System.Action<Scene> onWillSaveSceneEvent;
+
+    public static void OnWillSaveAssets(string[] names) {
+        for (int i = 0, len = names.Length; i < len; i++) {
+            string name = names[i];
+            if(StringUtil.EndsWith(name, SCENE_EXTENSION)) {
+                Scene scene = SceneManager.GetSceneByPath(name);
+                onWillSaveSceneEvent?.Invoke(scene);
+            }
+        }
+    }
+}
+#endif
