f81691a48b87546cb2a00a4cbd047eb8eed68753
kingBook <412126604@qq.com>
2022-07-06 12:16:42 +0800

    update 2022-07-06 12:16

--- Assets/Framework/Objs/Times/DelayDestroyer.cs
+++ Assets/Framework/Objs/Times/DelayDestroyer.cs
@@ -8,7 +8,13 @@ public class DelayDestroyer : MonoBehaviour {
 
     [Tooltip("时间（秒）")] public float time = 5;
 
+    /// <summary>
+    /// 销毁事件，回调函数格式：<code> void OnDestroyHandler() </code>
+    /// </summary>
+    public event System.Action onDestroyEvent;
+
     private void DestroySelf() {
+        onDestroyEvent?.Invoke();
         Destroy(gameObject);
     }
 
