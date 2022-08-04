e184dd393695ec46c497108b7d440d67594a6aed
kingBook <412126604@qq.com>
2022-08-04 15:41:54 +0800

    update 2022-08-04 15:41

--- Assets/Framework/Objs/Physics2D/CollideWallDetector2D.cs
+++ Assets/Framework/Objs/Physics2D/CollideWallDetector2D.cs
@@ -8,30 +8,49 @@ using UnityEngine;
 public class CollideWallDetector2D : MonoBehaviour {
 
     [Range(0f, 1f), Tooltip("左右是否撞墙的法线阀值，绝对值大于此值表示撞墙")] public float normalThresholdX = 0.7f;
+    [Range(0f, 1f), Tooltip("顶底是否撞墙的法线阀值，绝对值大于此值表示撞墙")] public float normalThresholdY = 0.7f;
+    public bool enabledCollideX = true;
+    public bool enabledCollideY = true;
 
     private readonly ContactPoint2D[] m_contacts = new ContactPoint2D[64];
     private Rigidbody2D m_rigidbody2D;
+    private Vector2Int m_collideSign;
 
     /// <summary>
-    /// -1：碰右墙；1：碰左墙
+    /// x: -1：碰右墙；1：碰左墙; y: -1：碰顶墙；1：碰底墙
     /// </summary>
-    public int collideSign { get; private set; }
+    public Vector2Int collideSign => m_collideSign;
 
     private void Awake() {
         m_rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate() {
-        int collideSignValue = 0;
         int count = m_rigidbody2D.GetContacts(m_contacts);
-        for (int i = 0; i < count; i++) {
-            var contact = m_contacts[i];
-            if (!contact.enabled) continue;
-            if (Mathf.Abs(contact.normal.x) > normalThresholdX) {
-                collideSignValue = (int)Mathf.Sign(contact.normal.x);
-                break;
+        if (enabledCollideX) {
+            int collideSignValue = 0;
+            for (int i = 0; i < count; i++) {
+                var contact = m_contacts[i];
+                if (!contact.enabled) continue;
+                if (Mathf.Abs(contact.normal.x) > normalThresholdX) {
+                    collideSignValue = (int)Mathf.Sign(contact.normal.x);
+                    break;
+                }
             }
+            m_collideSign.x = collideSignValue;
         }
-        collideSign = collideSignValue;
+
+        if (enabledCollideY) {
+            int collideSignValue = 0;
+            for (int i = 0; i < count; i++) {
+                var contact = m_contacts[i];
+                if (!contact.enabled) continue;
+                if (Mathf.Abs(contact.normal.y) > normalThresholdY) {
+                    collideSignValue = (int)Mathf.Sign(contact.normal.y);
+                    break;
+                }
+            }
+            m_collideSign.y = collideSignValue;
+        }
     }
 }
