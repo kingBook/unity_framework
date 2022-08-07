076bf58b8ff33157f828cfa6e3638c64e1618b8f
kingBook <412126604@qq.com>
2022-08-07 22:22:10 +0800

    update 2022-08-07 22:22

--- Assets/Scripts/EffectsFactory.cs
+++ Assets/Scripts/EffectsFactory.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class EffectsFactory : MonoBehaviour {
 
-    [SerializeField] private GameObject m_itemDestroyEffectPrefab;
+    //[SerializeField] private GameObject m_itemDestroyEffectPrefab;
 
     private GameObject CreateInstance(GameObject prefab, Transform parent, Vector3 position, Quaternion rotation) {
         var inst = Instantiate(prefab, parent);
@@ -12,9 +12,9 @@ public class EffectsFactory : MonoBehaviour {
     }
 
 
-    public GameObject CreateItemDestroyEffect(Transform parent, Vector3 position, Quaternion rotation) {
-        return CreateInstance(m_itemDestroyEffectPrefab, parent, position, rotation);
-    }
+    //public GameObject CreateItemDestroyEffect(Transform parent, Vector3 position, Quaternion rotation) {
+    //    return CreateInstance(m_itemDestroyEffectPrefab, parent, position, rotation);
+    //}
 
 
 }
