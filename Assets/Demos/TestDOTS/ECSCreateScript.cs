f9f80d58a01d2b75861f31ddb9a786439be93d0e
kingBook <412126604@qq.com>
2022-12-06 20:18:21 +0800

    update 2022-12-06 20:18

---
+++ Assets/Demos/TestDOTS/ECSCreateScript.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class ECSCreateScript : MonoBehaviour {
+
+    public Mesh mesh;
+    public Material material;
+    
+    public int MaxCount = 10000;
+    public float RandomNum = 10f;
+
+    void Start() {
+        World world = World.DefaultGameObjectInjectionWorld;
+        ECSMoveSystem moveSystem = world.GetOrCreateSystem<ECSMoveSystem>();
+        moveSystem.CreateAllEntity(MaxCount, RandomNum, mesh, material);
+        SimulationSystemGroup systemGroup = world.GetOrCreateSystem<SimulationSystemGroup>();
+        systemGroup.AddSystemToUpdateList(moveSystem);
+    }
+}
