4cabfaac398dba6798ed4b0ca9e79a75ae5d9d42
kingBook <412126604@qq.com>
2024-07-08 18:57:21 +0800

    update 2024-07-08 18:57

---
+++ Assets/Framework/Core/SkipSplashImage.cs
@@ -0,0 +1,16 @@
+﻿// 此脚本用于跳过Unity的启动Logo
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Scripting;
+
+[Preserve] // 此特性用于确保此脚本能被打包进程序
+public class SkipSplashImage {
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+    private static void Run() {
+        Task.Run(() => {
+            SplashScreen.Stop(SplashScreen.StopBehavior.StopImmediate);
+        });
+    }
+}
